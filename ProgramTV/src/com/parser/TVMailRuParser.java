fe016f478edcde6343deac7ecb6604b7a7abedfa
tonis38 <tonis38@o2.pl>
2018-05-23 13:18:47 +0200

    Fix

--- ProgramTV/src/com/parser/TVMailRuParser.java
+++ ProgramTV/src/com/parser/TVMailRuParser.java
@@ -1,11 +1,8 @@
 package com.parser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
