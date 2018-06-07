59e4f3ce50377f5d1ea289cd5efbb65c0100e0bb
tonis38 <tonis38@o2.pl>
2018-06-07 20:12:19 +0200

    Parser ready to use in database

--- ProgramTV/src/com/parser/TricolorTVParser.java
+++ ProgramTV/src/com/parser/TricolorTVParser.java
@@ -9,23 +9,29 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.teamEclipse.*;
-
 public class TricolorTVParser {
+	
+	private static List<TVItem> items = new LinkedList<TVItem>();
 
-  private static String readAll(Reader rd) throws IOException {
-    StringBuilder sb = new StringBuilder();
-    int cp;
-    while ((cp = rd.read()) != -1) {
-      sb.append((char) cp);
-    }
-    return sb.toString();
-  }
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
 
   public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
     InputStream is = new URL(url).openStream();
@@ -39,8 +45,17 @@ public class TricolorTVParser {
     }
   }
 
-  public static void main(String[] args) throws IOException, JSONException {
-    JSONObject json = readJsonFromUrl("https://www.tricolor.tv/ajax/program/grid.php?channel=select&channel-type=20&channel-name=0&channel-format=0&date_for_now=&day=&type=grid&favoriteChannels=undefined");
+  public static List<TVItem> ParseData(){
+    JSONObject json = null;
+	try {
+		json = readJsonFromUrl("https://www.tricolor.tv/ajax/program/grid.php?channel=select&channel-type=20&channel-name=0&channel-format=0&date_for_now=&day=&type=grid&favoriteChannels=undefined");
+	} catch (JSONException e) {
+		System.err.println("Cannot read JSON from site. \n");
+		e.printStackTrace();
+	} catch (IOException e) {
+		System.err.println("Cannot connect to site.");
+		e.printStackTrace();
+	}
     JSONObject progJson = json.getJSONObject("program");
     
     
@@ -49,24 +64,48 @@ public class TricolorTVParser {
     //Get all objects
     while(keys.hasNext()){
     	
-    	TVItem item = new TVItem();
+    	TVItem item = new TVItem();							//Create new item
    	
     	String key = keys.next();							//Get key
     	
     	JSONObject program = progJson.getJSONObject(key);	//Get object from key
     	
-    	//Get data from object
+    	DateFormat oldformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");		//Data format form JSON
+    	DateFormat newformat = new SimpleDateFormat("hh:mm dd-MM-yyyy");		//Data format in database
     	
-    	item.setNetwork(program.getString("channel"));
-       	item.setName(program.getString("title"));
-    	item.setAirDate(program.getString("start"));
+    	Date starttime = null;
+    	Date endtime = null;
+		try {
+			starttime = oldformat.parse(program.getString("start"));
+			endtime = oldformat.parse(program.getString("end"));
+		} catch (JSONException e) {
+			System.err.println("Cannot parse JSON file. \n");
+			e.printStackTrace();
+		} catch (ParseException e) {
+			System.err.println("cannot parse date. \n");
+			e.printStackTrace();
+		}
+    	
+    	String airdate = newformat.format(starttime);		//Get start date of program
+    	long duration = (endtime.getTime() - starttime.getTime()) / (1000 * 60);	//Get time duration of program
+    	
+    	String runtime = Long.toString(duration) + " minutes";
+    	
+    	//Add data to item
+    	item.setNetwork(program.getString("channelName"));
+       	item.setName(program.getString("title").replaceAll("&quot;", "'"));
+    	item.setAirDate(airdate);
+    	item.setRuntime(runtime);
     	item.setImage(program.getString("logo"));
     	item.setSummary(program.getString("descr"));
     	
+    	items.add(item);		//Add item to list of items
+    	System.out.println("Adding item......");
     	
-    	System.out.printf("Program name: %s. \t Start: %s.\n", item.getName(), item.getAirDate());
+    	
     }
-
     
+	return items;
+
   }
 }
