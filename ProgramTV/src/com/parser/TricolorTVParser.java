f7da31a26172cb19e1520ed627740c2813e19ed0
tonis38 <tonis38@o2.pl>
2018-06-12 22:50:44 +0200

    Integration with new methods

--- ProgramTV/src/com/parser/TricolorTVParser.java
+++ ProgramTV/src/com/parser/TricolorTVParser.java
@@ -13,16 +13,24 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class TricolorTVParser {
-	
-	private static List<TVItem> items = new LinkedList<TVItem>();
+public class TricolorTVParser extends Parser {
+
+	private List<TVItem> items;
+	private Set<TVNetwork> networks;
+
+	public TricolorTVParser() {
+		items = new LinkedList<TVItem>();
+		networks = new HashSet<TVNetwork>();
+	}
 
 	private static String readAll(Reader rd) throws IOException {
 		StringBuilder sb = new StringBuilder();
@@ -33,79 +41,85 @@ public class TricolorTVParser {
 		return sb.toString();
 	}
 
-  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-    InputStream is = new URL(url).openStream();
-    try {
-      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-      String jsonText = readAll(rd);
-      JSONObject json = new JSONObject(jsonText);
-      return json;
-    } finally {
-      is.close();
-    }
-  }
-
-  public static List<TVItem> ParseData(){
-    JSONObject json = null;
-	try {
-		json = readJsonFromUrl("https://www.tricolor.tv/ajax/program/grid.php?channel=select&channel-type=20&channel-name=0&channel-format=0&date_for_now=&day=&type=grid&favoriteChannels=undefined");
-	} catch (JSONException e) {
-		System.err.println("Cannot read JSON from site. \n");
-		e.printStackTrace();
-	} catch (IOException e) {
-		System.err.println("Cannot connect to site.");
-		e.printStackTrace();
-	}
-    JSONObject progJson = json.getJSONObject("program");
-    
-    
-    Iterator<String> keys = progJson.keys();
-    
-    //Get all objects
-    while(keys.hasNext()){
-    	
-    	TVItem item = new TVItem();							//Create new item
-   	
-    	String key = keys.next();							//Get key
-    	
-    	JSONObject program = progJson.getJSONObject(key);	//Get object from key
-    	
-    	DateFormat oldformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");		//Data format form JSON
-    	DateFormat newformat = new SimpleDateFormat("hh:mm dd-MM-yyyy");		//Data format in database
-    	
-    	Date starttime = null;
-    	Date endtime = null;
+	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
 		try {
-			starttime = oldformat.parse(program.getString("start"));
-			endtime = oldformat.parse(program.getString("end"));
-		} catch (JSONException e) {
-			System.err.println("Cannot parse JSON file. \n");
-			e.printStackTrace();
-		} catch (ParseException e) {
-			System.err.println("cannot parse date. \n");
-			e.printStackTrace();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			return json;
+		} finally {
+			is.close();
 		}
-    	
-    	String airdate = newformat.format(starttime);		//Get start date of program
-    	long duration = (endtime.getTime() - starttime.getTime()) / (1000 * 60);	//Get time duration of program
-    	
-    	String runtime = Long.toString(duration) + " minutes";
-    	
-    	//Add data to item
-    	item.setNetwork(program.getString("channelName"));
-       	item.setName(program.getString("title").replaceAll("&quot;", "'"));
-    	item.setAirDate(airdate);
-    	item.setRuntime(runtime);
-    	item.setImage(program.getString("logo"));
-    	item.setSummary(program.getString("descr"));
-    	
-    	items.add(item);		//Add item to list of items
-    	System.out.println("Adding item......");
-    	
-    	
-    }
-    
-	return items;
+	}
 
-  }
+	public void ParseData() {
+		JSONObject json = null;
+		try {
+			json = readJsonFromUrl(
+					"https://www.tricolor.tv/ajax/program/grid.php?channel=select&channel-type=20&channel-name=0&channel-format=0&date_for_now=&day=&type=grid&favoriteChannels=undefined");
+		} catch (JSONException e) {
+			System.err.println("Cannot read JSON from site. \n");
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			System.err.println("Cannot connect to site.");
+			e.printStackTrace();
+			return;
+		}
+
+		JSONObject progJson = json.getJSONObject("program");
+
+		Iterator<String> keys = progJson.keys();
+
+		// Get all objects
+		while (keys.hasNext()) {
+			String key = keys.next(); // Get key
+
+			JSONObject program = progJson.getJSONObject(key); // Get object from key
+
+			DateFormat oldformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); // Data format form JSON
+			DateFormat newformat = new SimpleDateFormat("hh:mm dd-MM-yyyy"); // Data format in database
+
+			Date starttime = null;
+			Date endtime = null;
+
+			try {
+				starttime = oldformat.parse(program.getString("start"));
+				endtime = oldformat.parse(program.getString("end"));
+			} catch (JSONException e) {
+				System.err.println("Cannot parse JSON file. \n");
+				e.printStackTrace();
+			} catch (ParseException e) {
+				System.err.println("cannot parse date. \n");
+				e.printStackTrace();
+			}
+
+			String airdate = newformat.format(starttime); // Get start date of program
+			long duration = (endtime.getTime() - starttime.getTime()) / (1000 * 60); // Get time duration of program
+
+			String runtime = Long.toString(duration) + " minutes";
+
+			// Add data to item
+			String network = program.getString("channelName");
+			String name = program.getString("title").replaceAll("&quot;", "'");
+			String image = program.getString("logo");
+			String summary = program.getString("descr");
+
+			items.add(new TVItem(0, name, 0, 0, airdate, runtime, image, summary, network, null));
+			System.out.println("Adding item......");
+
+		}
+
+		System.out.println("Parsing done...");
+
+	}
+
+	public List<TVItem> getItems() {
+		return items;
+	}
+
+	public Set<TVNetwork> getNetworks() {
+		return networks;
+	}
 }
